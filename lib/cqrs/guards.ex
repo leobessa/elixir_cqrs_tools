158e66fbb1fde45759d8896c2286b9e1b77b2017
leobessa <leobessa@gmail.com>
2022-06-17 11:52:46 -0300

    Update guards.ex

--- lib/cqrs/guards.ex
+++ lib/cqrs/guards.ex
@@ -1,26 +1,25 @@
 defmodule Cqrs.Guards do
   @moduledoc false
-  alias Cqrs.{InvalidCommandError, InvalidDispatcherError, InvalidQueryError}
 
-  def ensure_is_struct!(module) do
+  def ensure_is_struct!(_module) do
     # unless exports_function?(module, :__struct__, 0) do
     #   raise "#{module |> Module.split() |> Enum.join(".")} should be a valid struct."
     # end
   end
 
-  def ensure_is_command!(module) do
+  def ensure_is_command!(_module) do
     # unless exports_function?(module, :__command__, 0) do
     #   raise InvalidCommandError, command: module
     # end
   end
 
-  def ensure_is_query!(module) do
+  def ensure_is_query!(_module) do
     # unless exports_function?(module, :__query__, 0) do
     #   raise InvalidQueryError, query: module
     # end
   end
 
-  def ensure_is_dispatcher!(module) do
+  def ensure_is_dispatcher!(_module) do
     # unless exports_function?(module, :dispatch, 2) do
     #   raise InvalidDispatcherError, dispatcher: module
     # end
